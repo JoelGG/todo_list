a588b3c3c74f67fc31b98a58dde329aef3c07f4b
Joel Grimmer <joel.grimmer@gmail.com>
2020-04-14 17:15:14 +0100

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -44,14 +44,71 @@ class HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildRow (Task task) {
-    return ListTile(
-      title: Text(task.title + "," + task.date.toString()),
+    return Card(
+      child: ListTile(
+        title: Text(task.title),
+        subtitle: Text(task.date.toString()),
+        trailing: Container(
+          child: IconButton(
+            icon: Icon(Icons.check),
+            onPressed: () {
+              _removeTodo(task);
+            },
+          )
+        ),
+      )
     );
   }
 
   void _addTodos (Task t) {
+    showDialog(context: context, builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text("New Todo"),
+        content: Container(
+          height: 200.0,
+          width: 300.0,
+          child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: <Widget>[
+              Card(
+                child: TextField(
+                    decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Title',
+                  ),
+              ),
+              ),
+              Card(
+                child: InkWell(
+                  child: Text("Select Date"),
+                  onTap: () {showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(2001), lastDate: DateTime(2222));},
+                ),
+              ),
+              Card(
+                child: InkWell(
+                  child: Text("Select Date"),
+                  onTap: () {showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(2001), lastDate: DateTime(2222));},
+                ),
+              ),
+            ]
+          ),
+        ),
+        
+        actions: [
+          FlatButton(onPressed: () { Navigator.of(context).pop(); }, child: Text("Cancel")),
+          FlatButton(onPressed: null, child: Text("Accept")),
+        ],
+      );
+    });
+    
     setState(() {
       _todos.add(new Task(date: DateTime.now(), title: "Todo " + _todos.length.toString()));
+    });
+  }
+
+  void _removeTodo (Task t) {
+    setState(() {
+      _todos.remove(t);
     });
   }
 }
@@ -65,6 +122,14 @@ class HomeScreen extends StatefulWidget {
 class Task {
   String title;
   DateTime date;
+  Category category;
 
-  Task({this.title, this.date});
+  Task({this.title, this.date, this.category});
+}
+
+class Category {
+  Icon icon;
+  String name;
+
+  Category({this.icon, this.name});
 }
